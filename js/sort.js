fd87d172f7b2d68e9de7eb453f807adc28dd813a
hugcis <hmj.cisneros@gmail.com>
2021-08-20 16:54:52 +0200

    rebuilding site Fri Aug 20 16:54:52 CEST 2021

--- js/sort.js
+++ js/sort.js
@@ -1,24 +1,3 @@
-function search_function() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("search_input");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("note_list");
-    li = ul.getElementsByTagName('li');
-
-    // Loop through all list items, and hide those who don't
-    // match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
-
 function sort_by_rad(a, b) {
     let noteGraph = document.getElementsByClassName("note-graph");
     if (noteGraph.length == 0) {
