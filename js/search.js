f04e3c1c1f7b4c5bb34106b92ff62204815efa06
hugcis <hmj.cisneros@gmail.com>
2021-08-25 16:31:57 +0200

    rebuilding site Wed Aug 25 16:31:57 CEST 2021

--- js/search.js
+++ js/search.js
@@ -3,20 +3,26 @@ function search_function() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("search_input");
     filter = input.value;
-    const results = search(filter, 5).map((d) => d[0].toLowerCase());
 
     ul = document.getElementById("note_list");
     li = ul.getElementsByTagName('li');
-
-    // Loop through all list items, and hide those who don't
-    // match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByClassName("single-note-title")[0];
-        txtValue = a.textContent || a.innerText;
-        if (results.indexOf(txtValue.toLowerCase()) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+    if (filter.length == 0) {
+        for (let d of li) {
+           d.style.display = "";
+        }
+    }
+    else {
+        // Loop through all list items, and hide those who don't
+        // match the search query
+        const results = search(filter, 5).map((d) => d[0].toLowerCase());
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByClassName("single-note-title")[0];
+            txtValue = a.textContent || a.innerText;
+            if (results.indexOf(txtValue.toLowerCase()) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
         }
     }
 }
